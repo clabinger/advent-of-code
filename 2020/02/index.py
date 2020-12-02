a26bcb02d564334eb69c4822cacfd4bb6fe6e41a
Cooper Labinger <clabinger@gmail.com>
2020-12-02 06:49:52 -0500

    Add day 2, part 2

--- 2020/02/index.py
+++ 2020/02/index.py
@@ -4,12 +4,17 @@ sys.path.insert(0, '..')
 
 from library import input
 
-# Part 1: check that passwords meet their respective policies
+# Check that passwords meet their respective policies. The policies are different in Part 1 and Part 2.
 
 def check_policy_part_1(indicator: tuple, test_letter: str, password: str):
   # Return True if the number of instances of `test_letter` in `password` is within the range (inclusive) given by `indicator`
   occurences = password.count(test_letter)
   return indicator[0] <= occurences <= indicator[1]
+
+def check_policy_part_2(indicator: tuple, test_letter: str, password: str):
+  # Return True if `test_letter` appears in `password` in exactly one of the two positions indicated by `indicator` (1-indexed)
+  position_matches = sum(1 for position in range(2) if password[indicator[position] - 1] == test_letter)
+  return position_matches == 1
 
 def parse_line(input: str):
   # Extract `indicator`, `test_letter`, and `password` from a line
@@ -28,7 +33,10 @@ def parse_line(input: str):
     'indicator': tuple(map(int, m.group(1, 2)))
   }
   
-# Part 1 solution is the number of input lines that meet their respective policies
 print('Part 1 matches:',
   sum(1 for line in input() if check_policy_part_1(**parse_line(line)))
+)
+
+print('Part 2 matches:',
+  sum(1 for line in input() if check_policy_part_2(**parse_line(line)))
 )
